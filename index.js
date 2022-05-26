9f9a3c239658b93f2af0a487b63e2ed00f262005
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-05-27 00:03:19 +0600

    stripe add

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -51,6 +52,18 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
+
+    app.post('/create-payment-intent',verifyJWT,async(req, res)=>{
+      const product = req.body;
+      const price = product.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency : 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    })
 
     app.delete("/tools/:id", async (req, res) => {
       const id = req.params.id;
