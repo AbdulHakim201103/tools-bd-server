e494824ac60253437880ba69489c3baea69f3e45
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-06-01 23:46:30 +0600

    change

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ async function run() {
       res.send(result);
     });
 
+    // tools
     app.get("/tools/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
